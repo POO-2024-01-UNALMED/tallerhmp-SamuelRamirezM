4ddee90932d15d7c846adb2cab6acbdc643bb69e
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-24 18:44:13 -0500

    Clases independientes

--- Futbolista.py
+++ Futbolista.py
@@ -1,65 +1,16 @@
-class Persona:
-    #Inicializador y atributos
-    def __int__(self, nombre, edad, altura, sexo):
-        self._nombre = nombre
-        self._edad = edad
-        self._altura = altura
-        self._sexo = sexo
-
-    #Setters y getters
-    def setNombre(self, nombre):
-        self._nombre = nombre
-
-    def getNombre(self):
-        return self._nombre
-
-    def setEdad(self, edad):
-        self._edad = edad
-
-    def getEdad(self):
-        return self._edad
-
-    def setAltura(self, altura):
-        self._altura = altura
-
-    def getAltura(self):
-        return self._altura
-
-    def setSexo(self, sexo):
-        self._sexo
-
-    def getSexo(self):
-        return self._sexo
-
-class Deportista:
-    #Inicializador y atributos.
-    def __init__(self, deporte, añosPracticando):
-        self._deporte = deporte
-        self._añosPracticando = añosPracticando
-
-    def setDeporte(self, deporte):
-        self._deporte = deporte
-
-    def getDeporte(self):
-        return self._deporte
-
-    def setAñosPracticando(self, añosPracticando):
-        self._añosPracticando = añosPracticando
-
-    def getAñosPracticando(self):
-        return self._añosPracticando
-
+from Persona import Persona
+from Deportista import Deportista
 class Futbolista(Persona, Deportista):
     #Atributo de clase.
     listaFutbolistas = None
 
     #Inicializador y atributos.
-    def __init__(self, nombre, edad, altura, sexo, deporte, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
         Persona.__init__(self, nombre, edad, altura, sexo)
-        Deportista.__init__(self, deporte, añosPracticando)
+        Deportista.__init__(self, "Futbol", añosPracticando)
 
     def setGolesMarcados(self, golesMarcados):
         self._golesMarcados = golesMarcados
@@ -78,6 +29,14 @@ class Futbolista(Persona, Deportista):
 
     def getPiernaHabil(self):
         return self._piernaHabil
+
+    @classmethod
+    def setListaFutbolistas(cls, listaFutbolistas):
+        cls.listaFutbolistas = listaFutbolistas
+
+    @classmethod
+    def getListaFutbolistas(cls):
+        return cls.listaFutbolistas
 
     def __str__(self):
         return "Mi nombre es " + super().getNombre() + " soy profesional en el deporte " + super().getDeporte() + "Tengo " + super().getEdad() + " años de edad y llevo " + super().getAñosPracticando + "años en el deporte"
---
+++ Persona.py
@@ -0,0 +1,32 @@
+class Persona:
+    #Inicializador y atributos
+    def __int__(self, nombre, edad, altura, sexo):
+        self._nombre = nombre
+        self._edad = edad
+        self._altura = altura
+        self._sexo = sexo
+
+    #Setters y getters
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getNombre(self):
+        return self._nombre
+
+    def setEdad(self, edad):
+        self._edad = edad
+
+    def getEdad(self):
+        return self._edad
+
+    def setAltura(self, altura):
+        self._altura = altura
+
+    def getAltura(self):
+        return self._altura
+
+    def setSexo(self, sexo):
+        self._sexo
+
+    def getSexo(self):
+        return self._sexo
