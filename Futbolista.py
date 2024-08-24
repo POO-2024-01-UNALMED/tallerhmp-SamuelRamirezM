c6708cf62307527f4afcd12bb401b248725a6953
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-24 18:35:54 -0500

    Todo a una clase

--- Futbolista.py
+++ Futbolista.py
@@ -1,16 +1,65 @@
-import Persona
-import Deportista
+class Persona:
+    #Inicializador y atributos
+    def __int__(self, nombre, edad, altura, sexo):
+        self._nombre = nombre
+        self._edad = edad
+        self._altura = altura
+        self._sexo = sexo
+
+    #Setters y getters
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getNombre(self):
+        return self._nombre
+
+    def setEdad(self, edad):
+        self._edad = edad
+
+    def getEdad(self):
+        return self._edad
+
+    def setAltura(self, altura):
+        self._altura = altura
+
+    def getAltura(self):
+        return self._altura
+
+    def setSexo(self, sexo):
+        self._sexo
+
+    def getSexo(self):
+        return self._sexo
+
+class Deportista:
+    #Inicializador y atributos.
+    def __init__(self, deporte, añosPracticando):
+        self._deporte = deporte
+        self._añosPracticando = añosPracticando
+
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def getDeporte(self):
+        return self._deporte
+
+    def setAñosPracticando(self, añosPracticando):
+        self._añosPracticando = añosPracticando
+
+    def getAñosPracticando(self):
+        return self._añosPracticando
 
 class Futbolista(Persona, Deportista):
     #Atributo de clase.
     listaFutbolistas = None
 
     #Inicializador y atributos.
-    def __init__(self, golesMarcados, tarjetasRojas, piernaHabil):
+    def __init__(self, nombre, edad, altura, sexo, deporte, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
-        Persona.__init__()
+        Persona.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, deporte, añosPracticando)
 
     def setGolesMarcados(self, golesMarcados):
         self._golesMarcados = golesMarcados
